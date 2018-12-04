ac7b2288885a1c08edc98bc393af377a4674aa0f
madhusudhan reddy <33874664+mathrulestheworld@users.noreply.github.com>
2018-12-04 17:26:06 +0530

    pause added (#52)
    * added score variable
    
    * printing score on img
    
    * printing score
    
    * give points called
    
    * Update README.md
    
    * Update main.cpp
    
    * Update character.hpp
    
    * Update projectile.hpp
    
    * Update character.hpp
    
    * Update projectile.hpp
    
    * Update projectile.hpp
    
    * Update projectile.hpp
    
    * Update character.hpp
    
    * Update character.hpp
    
    * Update projectile.hpp
    
    * Update projectile.hpp
    
    * Update projectile.hpp
    
    * Update projectile.hpp
    
    * Update player.hpp
    
    * Update main.cpp
    
    * pause added

--- main.cpp
+++ main.cpp
@@ -53,7 +53,11 @@ int main()
 	Mat trans = img;
 	trans.setTo(Scalar(0, 0, 0));
 	while(p->keyInput(waitKey((int) delay)) && !escpressed)
-	{
+	{        if(pause==1)
+	    {putText(img, "pause", Point(150, 300), FONT_HERSHEY_SIMPLEX, 3, Scalar(255, 0, 0), 10, 2);
+	     imshow("Game", img);
+	     continue;
+	    }
 		img = imread(IMAGE,1);
 		printscore(p);
 		timeSinceFire = (dead==1)?fireRate:(double)(clock() - prevFire)/CLOCKS_PER_SEC;
